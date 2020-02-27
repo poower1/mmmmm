d5c51346c82d5fea5e0abec3c3a0f002a1a1c330
majid net <jhashem98@gmail.com>
2020-02-28 01:50:01 +0330

    ok

---
+++ New Text Document - Copy (2).js
@@ -0,0 +1,175 @@
+$(document).ready(function(){
+
+    $(".like").click(function(){
+        $(".like").toggleClass("like, red")
+
+    });
+
+
+    $(".like2").click(function(){
+        $(".like2").toggleClass("black, red")
+
+    });
+
+    $(".like3").click(function(){
+        $(".like3").toggleClass("black, red")
+
+    });
+
+    $(".like4").click(function(){
+        $(".like4").toggleClass("black, red")
+
+    });
+
+    $(".like5").click(function(){
+        $(".like5").toggleClass("black, red")
+
+    });
+
+    $(".like6").click(function(){
+        $(".like6").toggleClass("balck, red")
+
+    });
+
+    $(".like7").click(function(){
+        $(".like7").toggleClass("like, red")
+
+    });
+
+
+    $(".like8").click(function(){
+        $(".like8").toggleClass("black, red")
+
+    });
+
+    $(".like9").click(function(){
+        $(".like9").toggleClass("black, red")
+
+    });
+
+    $(".like10").click(function(){
+        $(".like10").toggleClass("black, red")
+
+    });
+
+    $(".like11").click(function(){
+        $(".like11").toggleClass("balck, red")
+
+    });
+    $(".like12").click(function(){
+        $(".like12").toggleClass("black, red")
+
+    });
+    $(".like13").click(function(){
+        $(".like13").toggleClass("black, red")
+
+    });
+    $(".like14").click(function(){
+        $(".like14").toggleClass("black, red")
+
+    });
+    $(".like15").click(function(){
+        $(".like15").toggleClass("black, red")
+
+    });
+    $(".like16").click(function(){
+        $(".like16").toggleClass("black, red")
+
+    });
+    $(".like17").click(function(){
+        $(".like17").toggleClass("black, red")
+
+    });
+    $(".like18").click(function(){
+        $(".like18").toggleClass("black, red")
+
+    });
+    $(".like19").click(function(){
+        $(".like19").toggleClass("black, red")
+
+    });
+    $(".like20").click(function(){
+        $(".like20").toggleClass("black, red")
+
+    });
+    $(".like21").click(function(){
+        $(".like21").toggleClass("black, red")
+
+    });
+    $(".like24").click(function(){
+        $(".like24").toggleClass("black, red")
+
+    });
+    $(".like22").click(function(){
+        $(".like22").toggleClass("black, red")
+
+    });
+    $(".like23").click(function(){
+        $(".like23").toggleClass("black, red")
+
+    });
+
+// comment
+
+
+    $(".comment").click(function(){
+        $(".commentin").toggle();
+    });
+
+
+    $(".comment2").click(function(){
+        $(".commentin2").toggle();
+    });
+
+    $(".comment3").click(function(){
+        $(".commentin3").toggle();
+    });
+
+    $(".comment4").click(function(){
+        $(".commentin4").toggle();
+    });
+
+    $(".comment5").click(function(){
+        $(".commentin5").toggle();
+    });
+
+    $(".comment6").click(function(){
+        $(".commentin6").toggle();
+    });
+
+    $(".comment7").click(function(){
+        $(".commentin7").toggle();
+    });
+
+    $(".comment8").click(function(){
+        $(".commentin8").toggle();
+    });
+
+    $(".comment9").click(function(){
+        $(".commentin9").toggle();
+    });
+
+    $(".comment10").click(function(){
+        $(".commentin10").toggle();
+    });
+
+    $(".comment11").click(function(){
+        $(".commentin11").toggle();
+    });
+
+    $(".comment12").click(function(){
+        $(".commentin12").toggle();
+    });
+
+
+    $("#musus").click(function(){
+        $(".buttonload").toggle();
+    });
+    $("#musus").click(function(){
+        $("#musus").hide();
+    });
+    $("#buttonload").click(function(){
+        alert(majiddd);
+    });
+});
+
